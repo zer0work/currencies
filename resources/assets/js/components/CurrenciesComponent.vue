7f96b8f54e919e19782599520c5827694a3c0338
Albert <albertwebdev@gmail.com>
2018-08-30 18:14:14 +0300

    load table via vuejs

--- resources/assets/js/components/CurrenciesComponent.vue
+++ resources/assets/js/components/CurrenciesComponent.vue
@@ -1,14 +1,32 @@
 <template>
-    <div class="container">
+    <div class="container mt-2">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Название валюты</th>
+                            <th>Цена</th>
+                            <th>Количество</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for='item in stock'>
+                            <td>{{item.name}}</td>
+                            <td>{{ parseInt(item.volume) }}</td>
+                            <td>{{ item.price.amount.toFixed(2) }}</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div class="col-md-2">
+                <button @click="update" class="btn btn-primary" v-if="!is_refresh">Обновить</button>
+                <span class="badge badge-primary" v-if="is_refresh">Обновление ...</span>
             </div>
         </div>
     </div>
@@ -16,8 +34,28 @@
 
 <script>
     export default {
+        data: function() {
+            return {
+                stock: [],
+                is_refresh: false,
+                id: 0
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.update()
+        },
+
+        methods: {
+            update: function () {
+                this.is_refresh = true
+                axios.get('/currencies').then((response) => {
+                    console.log(response);
+                    this.stock = response.data.stock
+                    this.is_refresh = false
+                    this.id++
+                });
+
+            }
         }
     }
 </script>
